c1056dde6bde7291136176d40d08076de71eb33d
Alec Reynolds <alec@kalamuna.com>
2016-03-04 16:22:27 -0800

    Basic express-ification of the website.

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-var http = require('http');
+var Twig = require('twig');
+var express = require('express');
+var app = express();
 var fs = require('fs');
 var formidable = require("formidable");
 var util = require('util');
 var request = require('request');
+var GoogleSpreadsheet = require("google-spreadsheet");
+var my_sheet = new GoogleSpreadsheet('1qzFyijCriIZcHqk-JuMg2lxL4fyMLRZybBPPZq2ECuM');
+var creds = require('./KalaboxKeycodes-ae31acf55c9a.json');
+var _ = require('lodash');
+var sass = require('node-sass');
 
-var server = http.createServer(function (req, res) {
+// App configuration
+app.set('view engine', 'twig');
+app.set('views', './views');
+app.use(sass.middleware({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/form', function (req, res) {
   // @todo: parse by download URL as well.
   try {
     if (req.method.toLowerCase() == 'post') {
@@ -13,27 +26,44 @@ var server = http.createServer(function (req, res) {
   } catch(err) {
     console.log(err);
   }
-
 });
+
+app.get('/', function (req, res) {res.render('home.twig');});
+app.get('/download.html', function (req, res) {res.render('download.twig');});
+app.get('/alpha-downloads', downloads);
+
+function downloads(req, res) {
+  var page = '';
+  if (!_.isEmpty(req.query.keycode) && /^(?:korobka|eske)-\w{6}$/.test(req.query.keycode)) {
+    page = 'alpha-downloads.twig';
+    saveSpreadsheet(req.query.keycode);
+  } else {
+    page = 'keycode-invalid.twig';
+  }
+
+  res.render(page, {
+    platform: 'Mac'
+  });
+};
+
+function saveSpreadsheet(keycode) {
+  my_sheet.useServiceAccountAuth(creds, function(err){
+    my_sheet.getRows(1, {query: 'kalabox = "' + keycode + '"'}, function( err, rows ){
+      _.forEach(rows, function(row) {
+        row.downloaded = true;
+        row.save();
+      })
+    });
+  });
+}
 
 function processAllFieldsOfTheForm(req, res) {
   var form = new formidable.IncomingForm();
 
   form.parse(req, function (err, fields, files) {
-    // @todo: does this pass validation?
-    var page = '';
-
-    if (/^(?:korobka|eske)-\w{6}$/.test(fields.keycode)) {
-      page = fs.readFileSync('alpha-downloads.html');
-    } else {
-      page = fs.readFileSync('keycode-invalid.html');
-    }
-
-    res.writeHead(200, {
-      'Content-Type': 'text/html',
-    });
-    res.end(page);
+    // Get keycode and pass to /alpha-downloads
+    request('/alpha-downloads?' + fields.keycode);
   });
 }
 
-server.listen(8080, '104.237.158.155');
+app.listen(8080);
