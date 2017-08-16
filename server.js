252f41dd5b6b939d1b32bc21df81423cb8fe8ae0
Mike Pirog <mike@thinktandem.io>
2017-08-15 19:05:48 -0700

    #81: Set the env on platform better

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash');
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -18,7 +19,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 // This will not replace current process.env eg you wont have to use this on Platform
 require('dotenv').config();
 
+// Load platform vars into the env if needed
+if (!_.isEmpty(process.env.PLATFORM_VARIABLES)) {
+  _.forEach(require('platformsh').config().variables, function(value, key) {
+    process.env[key] = value;
+  });
+}
+
 // Do prod things
+console.log(app.get('env'));
 if (app.get('env') === 'production') {
   port = require('platformsh').config().port;
   console.log(process.env);
